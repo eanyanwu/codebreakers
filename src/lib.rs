87801dce5b6b6c0154c051065afba78508db2020
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2020-05-24 21:57:48 -0400

    Implement letter frequency counting

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+mod common;
 pub mod errors;
 pub mod vigenere;
+pub mod letter_frequency;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::load_yaml;
 use clap::App;
 use codebreakers::vigenere;
+use codebreakers::letter_frequency;
 
 
 fn main() {
@@ -13,17 +14,22 @@ fn main() {
                 ("encipher", Some(encipher_cmd)) => {
                     let plain_text = encipher_cmd.value_of("plain-text").unwrap();
                     let key = encipher_cmd.value_of("key").unwrap();
-                    let cipher_text = vigenere::encipher(key, plain_text).unwrap();
+                    let cipher_text = vigenere::encipher(key.as_bytes(), plain_text.as_bytes()).unwrap();
                     println!("{}", cipher_text);
                 },
                 ("decipher", Some(decipher_cmd)) => {
                     let cipher_text = decipher_cmd.value_of("cipher-text").unwrap();
                     let key = decipher_cmd.value_of("key").unwrap();
-                    let plain_text = vigenere::decipher(key, cipher_text).unwrap();
+                    let plain_text = vigenere::decipher(key.as_bytes(), cipher_text.as_bytes()).unwrap();
                     println!("{}", plain_text);
                 },
                 _                            => {}
             }
+        },
+        ("letter-frequency", Some(letter_frequency_cmd)) => {
+            let text = letter_frequency_cmd.value_of("INPUT").unwrap();
+            let frequencies = letter_frequency::get_letter_frequency(text.as_bytes());
+            letter_frequency::print_histogram(&frequencies);
         }
         _                       => {}
     }
