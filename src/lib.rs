697db07419ff1f3c1e6d9a7c2d514906f95cf255
eanyanwu <ezeanyinabia.anyanwu@gmail.com>
2020-05-30 23:58:36 -0400

    Implement digram counting

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ mod common;
 pub mod errors;
 pub mod vigenere_standard;
 pub mod vigenere_autokey;
-pub mod letter_frequency;
+pub mod frequency;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::load_yaml;
 use clap::App;
 use codebreakers::vigenere_standard;
-use codebreakers::letter_frequency;
+use codebreakers::frequency;
 use std::io;
 use std::io::Read;
 
@@ -11,12 +11,12 @@ fn main() {
     let args = App::from_yaml(yaml).get_matches();
 
     match args.subcommand() {
-        ("vigenere", Some(vigenere)) => {
+        ("vigenere", Some(vigenere_cmd)) => {
             let mut input = Vec::new();
 
             io::stdin().read_to_end(&mut input).unwrap();
 
-            let output = match vigenere.subcommand() {
+            let output = match vigenere_cmd.subcommand() {
                 ("encipher", Some(encipher_cmd)) => {
                     let key = encipher_cmd.value_of("key").unwrap();
                     vigenere_standard::encipher(key.as_bytes(), &input).unwrap()
@@ -25,19 +25,30 @@ fn main() {
                     let key = decipher_cmd.value_of("key").unwrap();
                     vigenere_standard::decipher(key.as_bytes(), &input).unwrap()
                 },
-                _                            => { String::new() }
+                _ => { String::new() }
             };
 
             println!("{}", output);
         },
-        ("letter-frequency", Some(_)) => {
+        ("frequency", Some(frequency_cmd)) => {
             let mut text = Vec::new();
             io::stdin().read_to_end(&mut text).unwrap();
 
-            let frequencies = letter_frequency::get_letter_frequency(&text);
+            match frequency_cmd.subcommand() {
+                ("single", Some(_)) => {
+                    let frequencies = frequency::single_letter(&text);
+        
+                    frequency::print_single_letter_histogram(&frequencies);
+                },
+                ("digram", Some(_)) => {
+                    let frequencies = frequency::digram(&text);
 
-            letter_frequency::print_histogram(&frequencies);
+                    frequency::print_digram_frequencies(&frequencies);
+                },
+                _ => {}
+            }
+
         }
-        _                       => {}
+        _ => {}
     }
 }
